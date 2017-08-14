13ab8a1d609a254fbfca739ebb93e6c250c2107a
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-14 01:33:41 -0400

    completed total_times

--- total_times.rb
+++ total_times.rb
@@ -1,9 +1,24 @@
-count = 0
 roll = [1,2,3,4,5,6]
+total_times = {
+  2 => 0,
+  3 => 0,
+  4 => 0,
+  5 => 0,
+  6 => 0,
+  7 => 0,
+  8 => 0,
+  9 => 0,
+  10 => 0,
+  11 => 0,
+  12 => 0
+}
 roll.each do |num|
   roll.each do |num2|
-    count = count + 1
-    puts "Dice Roll:#{num} , #{ num2} Total: #{num + num2}"
+    total = num + num2
+    total_times [total] = total_times[total] + 1
   end
 end
-  puts "There are #{count} possible permutations"
+
+total_times.each do |occurs,n_times|
+  puts "#{occurs} occurs #{n_times} times"
+end
